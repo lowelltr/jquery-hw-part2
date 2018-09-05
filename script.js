7a2f64975b3d4d66e449964661a91d8d5b37e1d4
Tremell Lowell <tremelljlowell@gmail.com>
2018-09-05 03:47:16 -0400

    updated css

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 "use strict";
 $(() => {
   let currentTable = null;
-$(".Guestinfo").hide()
 
-  for (let i = 0; i < 9; i++) {
-    $("#table").append(`<div>${(i+1)}</div>`);
+
+   for (let i = 0; i < 9; i++) {
+   $("#table").append(`<div>${(i+1)}</div>`);
   }
 
   $("#table div").attr("class","tabledivs available");
 
   $("body").on("click", "#table div.available", (e) => {
     $("#seatform").show();
-    console.log(e.target);
     $("#seatform #seatforminfo form p").text(`Table Number: ${$(e.target).text()}`);
     currentTable = $(e.target);
   });
@@ -22,31 +21,31 @@ $(".Guestinfo").hide()
       currentTable
         .removeClass("available")
         .addClass("reserved")
-        .data( "guest-name",$("input").eq(0).val())
+        .data("guest-name",$("input").eq(0).val())
         .data("number-of-guests",$("input").eq(2).val())
         .data("guest-phone-mumber",$("input").eq(1).val())
         console.dir(currentTable.data("number-of-guests"));
     }
   });
-  //$("body").on("mouseenter mouseleave", "#table div.reserved, section.guest-details", (e) => {
-    //$(".guest-details").toggle();
-    //$(e.target).css("cursor", "not-allowed");
-
 
     $("body").on("mouseenter", "#table div.reserved", (e) => {
      $(e.target).css("cursor", "not-allowed");
       let name=$(e.target).data("guest-name")
       let party=$(e.target).data('number-of-guests')
   
-    $(".Guestinfo").append(`<p>table name,${name}</p> <p>size of party,${party}</p>`)
-    $(".Guestinfo").show()
-   
-    });
+    $(e.target).prepend(`<p>guest name:${name},size of party:${party}</p>`)
 
+
+    }) .on("mouseleave","#table div.reserved",(e)=>{
+        $("p").remove()
+        
+
+
+
+    
   });
-     $("body").on("mouseleave","#table div.reserved",(e)=>{
-     $(".Guestinfo").hide()
-
-   });
+    
+});
+  
 
 
